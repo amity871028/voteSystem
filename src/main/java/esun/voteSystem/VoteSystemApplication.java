61fc07ae362fde8f8114ef6dcda5e51ad3c3e98f
林怡萱 <amity@linyixuandeMacBook-Air.local>
2023-10-03 10:10:07 +0800

    feat: initialize the project

---
+++ src/main/java/esun/voteSystem/VoteSystemApplication.java
@@ -0,0 +1,13 @@
+package esun.voteSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/esun/voteSystem/VoteSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package esun.voteSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
